8b4564c5468e6b8ae317e057594f91a66dfb8745
Honghanhi <117724583+Honghanhi@users.noreply.github.com>
2026-04-08 20:11:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://proof-ai-one.vercel.app"
+        "https://proof-ai-one.vercel.app",
         'http://127.0.0.1:5500',      
         'http://localhost:5500',       
         'http://localhost:3000',      
